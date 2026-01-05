53e0f2c9d75a2083de22045e7bb5cb36aaec459f
Rahul Mac <appsrahulgautam@gmail.com>
2026-01-06 03:21:36 +0530

    v6

--- prisma.config.js
+++ prisma.config.js
@@ -1,15 +1,12 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 require("dotenv/config");
-const config_1 = require("prisma/config");
-exports.default = (0, config_1.defineConfig)({
-    schema: "prisma/schema.prisma",
-    migrations: {
-        path: "prisma/migrations",
-        // This tells Prisma how to execute your seed file
-        seed: "ts-node prisma/seed.ts",
-    },
-    datasource: {
-        url: process.env["DATABASE_URL"],
-    },
-});
+
+/** @type {import('prisma').Config} */
+module.exports = {
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
+  datasource: {
+    url: process.env.DATABASE_URL,
+  },
+};
